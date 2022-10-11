1b07ce6bc906a05e242fbfa0cd902ed9387489c8
EleniPP <eleni1997pp@gmail.com>
2022-10-11 16:07:50 +0300

    hi

--- memory.py
+++ memory.py
@@ -1,13 +1,7 @@
 #storing and retreiving files from memory
 from ast import keyword
 import networkx as nx
-
-
-class long_term_mem:
-    def __init__(self,keyword, emotion_array):
-        self.keyword = keyword
-        self.emotion = emotion_array
-
+import json
 class memory:
     def __init__(self, userName):
         try:
@@ -18,12 +12,16 @@ class memory:
             self.long_term = nx.Graph() #this still needs saving
         self.short_term = []
 
-    def process_input_to_mem(list_of_key_words: list[long_term_mem]):
-        
-def build_memory_graph():
-    G = nx.Graph()
-def add_to_memory(keyword_dict):
-    i = 0
-    for key in keyword_dict:
-        G.add_node(i, key = key, emotion = keyword_dict[key])
-        i += 1
+    # def process_input_to_mem(list_of_key_words: list[long_term_mem]):
+
+# def build_memory_graph():
+#     G = nx.Graph()
+    def add_to_memory(self, keyword_dict):
+        i = 0
+        for key in keyword_dict:
+            # check if the word is in graph
+            # add it or fuse it
+            # add the new connections
+            self.long_term.add_node(i, key = key, emotion = keyword_dict[key])
+            i += 1
+
