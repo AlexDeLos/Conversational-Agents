3df81bf25a4695afddcb5b4bb93a7d8e81d0cc58
AlexDeLos <alexdelos2001@gmail.com>
2022-10-11 13:28:10 +0200

    this branch will be used for memory

--- memory.py
+++ memory.py
@@ -1,4 +1,14 @@
 #storing and retreiving files from memory
+from ast import keyword
 
-def store_to_long_term():
+
+class long_term_mem:
+    def __init__(self,keyword, emotion_array):
+        self.keyword = keyword
+        self.emotion = emotion_array
+
+
+def store_to_long_term(list: list[long_term_mem]):
+    for mem in list:
+        
     print("storing to long term")
