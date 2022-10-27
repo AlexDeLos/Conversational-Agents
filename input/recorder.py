79951e26c1fdf12e5d03b472fba8f3d1838d1fec
Ilinca <i.e.i.rentea-1@student.tudelft.nl>
2022-10-27 17:50:46 +0200

    changed main to use furhat

--- input/recorder.py
+++ input/recorder.py
@@ -1,3 +1,4 @@
+from time import time
 from input.constants import CHANNELS, RMS_THRESHOLD, SAMPLE_RATE, SILENCE_THRESHOLD, TIME_THRESHOLD
 from dataclasses import dataclass
 from input.asr import ASR, ModelType
@@ -8,7 +9,7 @@ import numpy as np
 import sounddevice as sd
 import soundfile as sf
 import os, shutil, uuid
-import pickle
+import pickle, time
 
 @dataclass
 class RMS:
@@ -16,7 +17,7 @@ class RMS:
     silent: bool
 
 class Recorder:
-    def __init__(self, queue: Queue, model_type: ModelType = ModelType.SMALL):
+    def __init__(self, queue: Queue, model_type: ModelType = ModelType.BASE):
         self.queue = queue
         self.asr = ASR(model_type)
         self.data = []
@@ -31,13 +32,14 @@ class Recorder:
         Process(target=self.record_and_process).start()
 
     def record_and_process(self) -> None:
-        def callback(indata, frames, time, status) -> None:
-            self.last_time = time.currentTime
+        def callback(indata, frames, _time, status) -> None:
+            self.last_time = time.time()
             self.data.extend(indata[:, 0])
-            self.window.append(RMS(time.currentTime, np.sqrt(np.mean(indata[:, 0] ** 2)) < RMS_THRESHOLD))
+            self.window.append(RMS(self.last_time, np.sqrt(np.mean(indata[:, 0] ** 2)) < RMS_THRESHOLD))
 
         self.stream = sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS, callback=callback)
         
+        print("Recording")
         while True:
             self.stream.start()
             self.process_data()
@@ -50,7 +52,7 @@ class Recorder:
         percentage_of_silence = sum([ o.silent for o in self.window ]) / len(self.window)
 
         # Check if there is a silence and there were at least samples for `TIME_THRESHOLD` seconds.
-        if percentage_of_silence > SILENCE_THRESHOLD and len(self.window) > TIME_THRESHOLD * 128 * 0.75:
+        if percentage_of_silence > SILENCE_THRESHOLD and len(self.window) > 15:
             silent_array = [ o.silent for o in self.window ]
             data = self.data[silent_array.index(False):] if False in silent_array else []
 
@@ -68,7 +70,7 @@ def process_audio_data(asr: ASR, queue: Queue, data, ser) -> None:
 
     text = asr.transcribe(wav_file)
 
-    os.system(f"ffmpeg -i {wav_file} -ac 1 -ar 16000 {wav_file.replace('-', '_')} >/dev/null 2>&1")
+    os.system(f"ffmpeg -i {wav_file} -ac 1 -ar 16000 {wav_file.replace('-', '_')}")
     emotion = ser.predict_proba(extract_feature(wav_file.replace('-', '_'), mfcc = True, chroma = True, mel = True).reshape(1,-1))
     
     queue.put((text, emotion))
--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 from input.recorder import Recorder
-from multiprocessing import Queue
+from multiprocessing import Queue, Process
+from furhat_remote_api import FurhatRemoteAPI
+from input.ser import extract_feature
+import sounddevice as sd
+import pickle, uuid
+import soundfile as sf
+from scipy.io.wavfile import write
 
 def main():
-    data_queue = Queue()
-    recorder = Recorder(data_queue)
-    recorder.start()
+    # data_queue = Queue()
+    # recorder = Recorder(data_queue)
+    # recorder.start()
+
+    ser = pickle.load(open("input/result/mlp_classifier.model", "rb"))
+
+    furhat = FurhatRemoteAPI("localhost")
+
+    # Get the voices on the robot
+    # voices = furhat.get_voices()
+
+    # Set the voice of the robot
+    furhat.set_voice(name='Matthew')
+
+    # Say "Hi there!"
+    furhat.say(text="Hi there!Please state your name.")
+    name = furhat.listen()
+    print(name.message)
+    furhat.say(text="Thank you! What do you want to talk about?")
+    wav_file = f'tmp/{str(uuid.uuid4())}.wav'
 
     while True:
-        if data_queue.empty():
-            continue
+        
+        result = furhat.listen()
+        print(result.message)
 
-        print(data_queue.get())
+        myrecording = sd.rec(int(3*16000), samplerate=16000, channels=1, dtype="float32")
+        sd.wait()   
+        write(wav_file, 16000, myrecording)
+        emotion = ser.predict_proba(extract_feature(wav_file, mfcc = True, chroma = True, mel = True).reshape(1,-1))  
+        furhat.say(wav_file)
 
+        print(emotion)
+        furhat.say(text = "Can you please repeat that?")
+    
 if __name__ == "__main__":
     main()
