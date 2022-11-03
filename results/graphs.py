9d8ee5930a236c5efd9ed10e70ebef29e49dcf25
Thijs Nulle <thijsnulle@Thijss-MacBook-Pro.local>
2022-11-03 10:10:28 +0100

    Thijs: added data points

--- results/graphs.py
+++ results/graphs.py
@@ -7,15 +7,15 @@ import seaborn as sns
 f, ax = plt.subplots(figsize=(7, 6))
 
 data = [
-    { 'statement': 'The system is useful.', 'true': [3,	4, 3, 4, 5, 3, 3, 2, 3], 'false': [4, 4, 5, 2, 5, 4, 3, 3, 4]},
-    { 'statement': 'The system is pleasant.', 'true': [6, 5, 5, 6, 7, 5, 3, 4, 4], 'false': [6, 5, 6, 6, 7, 7, 4, 5, 5]},
-    { 'statement': 'The system is friendly.', 'true': [5, 4, 6, 7, 7, 6, 7, 4, 3], 'false': [6, 5, 6, 7, 7, 7, 6, 6, 7]},
-    { 'statement': 'I was able to recover easily from errors.', 'true': [3,	3, 3, 3, 2, 5, 4, 5, 2], 'false': [3, 3, 3, 3, 2, 5, 5, 5, 6]},
-    { 'statement': 'I enjoyed using the system.', 'true': [4, 5, 6, 7, 7, 6, 3, 4, 3], 'false': [6, 6, 6, 6, 7, 7, 6, 3, 5]},
-    { 'statement': 'It is clear how to speak to the system.', 'true': [6, 5, 6, 7, 7, 7, 4, 6, 5], 'false': [6, 5, 6, 7, 7, 7, 6, 6, 6]},
-    { 'statement': 'It is easy to learn to use the system.', 'true': [5, 6,	6, 7, 7, 6, 4, 6, 4], 'false': [7, 6, 6, 7, 7, 6, 6, 6, 6]},
-    { 'statement': 'I would use this system.', 'true': [3, 4, 4, 2, 3, 5, 2, 3, 2], 'false': [3, 4, 4, 3, 3, 5, 4, 2, 4]},
-    { 'statement': 'I felt in control of the interaction with the system.', 'true': [6,	5, 6, 5, 6, 6, 5, 3, 4], 'false': [7, 6, 7, 5, 6, 7, 5, 5, 6]},
+    { 'statement': 'The system is useful.', 'true': [3,	4, 3, 4, 5, 3, 3, 2, 3, 3, 4, 2], 'false': [4, 4, 5, 2, 5, 4, 3, 3, 4, 4, 5, 3]},
+    { 'statement': 'The system is pleasant.', 'true': [6, 5, 5, 6, 7, 5, 3, 4, 4, 5, 5, 4], 'false': [6, 5, 6, 6, 7, 7, 4, 5, 5, 5, 5, 6]},
+    { 'statement': 'The system is friendly.', 'true': [5, 4, 6, 7, 7, 6, 7, 4, 3, 4, 5, 6], 'false': [6, 5, 6, 7, 7, 7, 6, 6, 7, 6, 6, 7]},
+    { 'statement': 'I was able to recover easily from errors.', 'true': [3,	3, 3, 3, 2, 5, 4, 5, 2, 3, 2, 3], 'false': [3, 3, 3, 3, 2, 5, 5, 5, 6, 4, 3, 4]},
+    { 'statement': 'I enjoyed using the system.', 'true': [4, 5, 6, 7, 7, 6, 3, 4, 3, 4, 4, 5], 'false': [6, 6, 6, 6, 7, 7, 6, 3, 5, 5, 5, 6]},
+    { 'statement': 'It is clear how to speak to the system.', 'true': [6, 5, 6, 7, 7, 7, 4, 6, 5, 5, 4, 6], 'false': [6, 5, 6, 7, 7, 7, 6, 6, 6, 6, 6, 7]},
+    { 'statement': 'It is easy to learn to use the system.', 'true': [5, 6,	6, 7, 7, 6, 4, 6, 4, 5, 5, 6], 'false': [7, 6, 6, 7, 7, 6, 6, 6, 6, 6, 7, 6]},
+    { 'statement': 'I would use this system.', 'true': [3, 4, 4, 2, 3, 5, 2, 3, 2, 3, 4, 3], 'false': [3, 4, 4, 3, 3, 5, 4, 2, 4, 4, 4, 4]},
+    { 'statement': 'I felt in control of the interaction with the system.', 'true': [6,	5, 6, 5, 6, 6, 5, 3, 4, 5, 4, 5], 'false': [7, 6, 7, 5, 6, 7, 5, 5, 6, 6, 6, 5]},
 ]
 
 ### TRANSFORM AND PLOT INPUT DATA ###
@@ -34,7 +34,7 @@ sns.boxplot(data=mdf, y='Statement', x='Score', hue="Memory", palette="Blues")
 ### ADD MEMORY LEGEND ###
 
 handles, labels = ax.get_legend_handles_labels()
-memory_legend = ax.legend(handles=handles, labels=['With Memory', 'Without Memory'], bbox_to_anchor=(.35, 1.0))
+memory_legend = ax.legend(handles=handles, labels=['With Emotion Model', 'Without Emotion Model'], bbox_to_anchor=(.35, 1.0))
 ax.add_artist(memory_legend)
 
 ### ADD SCORE LEGEND ###
